5fd8c9bdea260841760662e68b577dc461d0455a
Vinicios Pires <contato.viniciospires@gmail.com>
2022-02-03 17:10:15 -0300

    Adição do giro da carta

--- script/script.js
+++ script/script.js
@@ -1,24 +1,22 @@
 let numCartas;
+let jogadas = 0;
 
 function addCartas() {
-    // while (numCartas < 4 && numCartas > 14 && numCartas % 2 !== 0) {
-    //     numCartas = parseInt(prompt("Com quantas cartas você quer jogar? (4 até 14 cartas)"))
-    // }
     while (!(numCartas <= 14 && numCartas >= 4 && numCartas % 2 === 0)) {
         numCartas = parseInt(prompt("Com quantas cartas você quer jogar? (4 até 14 cartas)"));
     }
 
     let imgs = [
-        'imgs/bobrossparrot.gif',
-        'imgs/explodyparrot.gif',
-        'imgs/fiestaparrot.gif',
-        'imgs/metalparrot.gif',
-        'imgs/revertitparrot.gif',
-        'imgs/tripletsparrot.gif',
-        'imgs/unicornparrot.gif',
+        '/projeto4-parrotscardgame/imgs/bobrossparrot.gif',
+        '/projeto4-parrotscardgame/imgs/explodyparrot.gif',
+        '/projeto4-parrotscardgame/imgs/fiestaparrot.gif',
+        '/projeto4-parrotscardgame/imgs/metalparrot.gif',
+        '/projeto4-parrotscardgame/imgs/revertitparrot.gif',
+        '/projeto4-parrotscardgame/imgs/tripletsparrot.gif',
+        '/projeto4-parrotscardgame/imgs/unicornparrot.gif'    
     ]
 
-    imgs.sort(comparador);
+    // imgs.sort(comparador);
 
     let par = [];
 
@@ -32,13 +30,21 @@ function addCartas() {
 
     for (let i = 0; i < numCartas; i++) {
         cartas.innerHTML += `<div onclick="virarCarta(this)" class="card" data-identifier="card">
-        <div class="front-face corpo-carta" data-identifier="front-face">
-        <img src="${par[i]}"></img>
-        </div>
-        <div class="back-face corpo-carta" data-identifier="back-face"></div>
+        <div class="front-face corpo-carta" data-identifier="front-face"> <img src="${par[i]}" /> </div>
+        <div class="back-face corpo-carta" data-identifier="back-face"> </div>
         </div>
         `
     }
 }
 
+function virarCarta(cartaEscolhida) {
+    jogadas++
+    cartaEscolhida.classList.add(".escolhida")
+}
+
+
+
+function comparador() {
+    return Math.random() - 0.5;
+}
 addCartas()
